db2db250d01e4d81fa87f1cf9dd4d7211d9972b3
Seogba <101324621+Seogba@users.noreply.github.com>
2022-09-11 02:19:34 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -5,48 +5,49 @@ import axios from 'axios'
 
 
 
+
 function App() {
   interface data{
     accessId : string,
     nickname:string,
     level:number
-  }
+  } 
+  
+  const [playerName , setPlayerName] = useState<string>("");
+  const [playerData , setPlayerData] = useState<any>([]);
+  const [dataExist , setDataExist] = useState<boolean>(false);
+  
 
-  
-  
-  const [playerName , setPlayerName] = useState<string>("잭팟풀")
-  
-  // useEffect(() => {
-  //   axios(`https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=${playerName}`).then((res)=>{
-  //     console.log("res.data" , res.data.json)
-  //   }).catch((err) => {
-  //     console.log(err);
-  //   })
-  // },[playerName]);
+  //유저의 닉네임을 통해 고유 식별자 , 레벨 등을 가져온다
 
-  useEffect(()=>{
-    axios({
-      url: `https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=${playerName}`,
-      method: "GET",
-      headers:{
-        "KeyName":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjE2Nzc5Nzk4NzciLCJhdXRoX2lkIjoiMiIsImV4cCI6MTY3ODEyMjkxNiwiaWF0IjoxNjYyNTcwOTE2LCJuYmYiOjE2NjI1NzA5MTYsInNlcnZpY2VfaWQiOiI0MzAwMTE0ODEiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.EMMb3J8HgjFCM6Qzfg2DpbSfW0k506S4HTMgpxRYqbs"
-      },
-    }).then((res)=>
-    {console.log("res.data: ",res.data.json)}).catch((err) =>{
+  const callUserData = (() => {
+    axios(
+      `https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=${playerName}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6Ijk0MDE1OTM4MSIsImF1dGhfaWQiOiIyIiwiZXhwIjoxNjc4MzcyMTg0LCJpYXQiOjE2NjI4MjAxODQsIm5iZiI6MTY2MjgyMDE4NCwic2VydmljZV9pZCI6IjQzMDAxMTQ4MSIsInRva2VuX3R5cGUiOiJBY2Nlc3NUb2tlbiJ9.leauru723Bw7SKcQPlwbXUibcT280fU3u2YLm9R8Cjc"
+        },
+      }
+    ).then((res) => {
+      console.log("res.data" , res.data);
+      setPlayerData(res.data);
+      setDataExist(true);
+    }).catch((err) => {
       console.log(err);
-    })
-    },[playerName]);
+    });
+  })
 
-//  useEffect(()=>{
-//   fetch(`https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname={playerName}`).then((res) => res.json)
-//  })
+  
 
   return (
     <div className="App">
-      <div>
+      <div className='search-user'>
       <input type="text" className='input-nickname'
       placeholder='닉네임' onChange={(event:React.ChangeEvent<HTMLInputElement>)=>setPlayerName(event.target.value)} />
+      <button onClick={callUserData}>검색</button>
       </div>
+      <div className='user-data'></div>
     </div>
   );
 }
---
+++ src/Component/BottomNav.tsx
@@ -0,0 +1,14 @@
+
+
+const BottomNav = () => {
+
+    return(
+        <div>
+            <div className="BottomNav">
+                <h2>Data based on NEXON DEVELOPERS</h2>
+            </div>
+        </div>
+    )
+};
+
+export default BottomNav;
